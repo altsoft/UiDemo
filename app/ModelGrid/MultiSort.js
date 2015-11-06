59f101b98ec79d624e4ffdf4ff958646da62313c
mg <mg@altsoft.biz>
2015-11-06 15:53:20 +0300

    sorting in SimpleGrid and in MultiSort

--- app/ModelGrid/MultiSort.js
+++ app/ModelGrid/MultiSort.js
@@ -19,34 +19,40 @@ function MultiSort() {
     widget.height = 120;
     
     var grid = new P.ModelGrid();
-    
     grid.editable = grid.deletable = grid.insertable = false;
-    var colService = new P.ServiceGridColumn();
+    
     var colName = new P.ModelGridColumn();
     colName.title = "Pet's name";
     colName.minWidth = 107;
+    colName.sortable = true;
     colName.field = 'pet'; 
     colName.editor = new P.ModelCombo();
     colName.editor.displayField = 'name';
+    
     var colFromDate = new P.ModelGridColumn();
     colFromDate.title = 'From date';
     colFromDate.minWidth = 107;
+    colFromDate.sortable = true;
     colFromDate.field = 'fromdate';
     colFromDate.editor = new P.ModelDate();
     colFromDate.editor.format = 'dd.MM.yyyy';
+    
     var colToDate = new P.ModelGridColumn();
     colToDate.title = 'To date';
     colToDate.minWidth = 107;
-    colToDate.field = 'todate';
+    colToDate.sortable = true;
+    colToDate.field = 'todate';    
     colToDate.editor = new P.ModelDate();
     colToDate.editor.format = 'dd.MM.yyyy';
+    
     var colIsPaid = new P.ModelGridColumn();
     colIsPaid.title = 'Paid';
     colIsPaid.minWidth = 65;
+    colIsPaid.sortable = true;
     colIsPaid.field = 'ispaid';
     colIsPaid.editor = new P.ModelCheckBox();
     
-    grid.addColumnNode(colService);
+    grid.addColumnNode(new P.ServiceGridColumn());
     grid.addColumnNode(colName);
     grid.addColumnNode(colFromDate);
     grid.addColumnNode(colToDate);
@@ -60,7 +66,6 @@ function MultiSort() {
             grid.data = model.qAllVisits;
         });
     };
-
 
     self.getDemoComponent = function () {
         return widget;
