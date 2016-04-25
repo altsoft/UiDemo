0e5c2f7e36b846f97ba856a4902b409e3fe94400
jskonst <jskonst@yandex.ru>
2016-04-25 17:02:40 +0300

    changes in build.sh

--- build-config/build.sh
+++ build-config/build.sh
@@ -1,20 +1,37 @@
 #!/bin/sh
-r.js -o build.js
-java -jar ~/Platypus/bin/Tools.jar -minify -app-folder ~/workspace/UiDemo/app/StandardWidgets -processed-folder ~/workspace/UiDemo/app/StandardWidgets -minified-models ~/workspace/uiCompressed/app/StandardWidgets/Modules.model -minified-layouts ~/workspace/uiCompressed/app/StandardWidgets/Modules.layout
-java -jar ~/Platypus/bin/Tools.jar -minify -app-folder ~/workspace/UiDemo/app/ModelWidgets -processed-folder ~/workspace/UiDemo/app/ModelWidgets -minified-models ~/workspace/uiCompressed/app/ModelWidgets/Modules.model -minified-layouts ~/workspace/uiCompressed/app/ModelWidgets/Modules.layout
-java -jar ~/Platypus/bin/Tools.jar -minify -app-folder ~/workspace/UiDemo/app/ModelGrid -processed-folder ~/workspace/UiDemo/app/ModelGrid -minified-models ~/workspace/uiCompressed/app/ModelGrid/Modules.model -minified-layouts ~/workspace/uiCompressed/app/ModelGrid/Modules.layout
-java -jar ~/Platypus/bin/Tools.jar -minify -app-folder ~/workspace/UiDemo/app/MenuWidgets -processed-folder ~/workspace/UiDemo/app/MenuWidgets -minified-models ~/workspace/uiCompressed/app/MenuWidgets/Modules.model -minified-layouts ~/workspace/uiCompressed/app/MenuWidgets/Modules.layout
-java -jar ~/Platypus/bin/Tools.jar -minify -app-folder ~/workspace/UiDemo/app/ContainerPanels -processed-folder ~/workspace/UiDemo/app/ContainerPanels -minified-models ~/workspace/uiCompressed/app/ContainerPanels/Modules.model -minified-layouts ~/workspace/uiCompressed/app/ContainerPanels/Modules.layout
-java -jar ~/Platypus/bin/Tools.jar -minify -app-folder ~/workspace/UiDemo/app/Utils -processed-folder ~/workspace/UiDemo/app/Utils -minified-models ~/workspace/uiCompressed/app/Utils/Modules.model -minified-layouts ~/workspace/uiCompressed/app/Utils/Modules.layout
+echo "Copying files"
+COMPRESSED_FOLDER=~/workspace/uiCompressed
+SOURCE_FOLDER=~/workspace/UiDemo
+TOOLS=~/Platypus/bin/Tools.jar
+cp -r $SOURCE_FOLDER/META-INF $COMPRESSED_FOLDER/
+cp -r $SOURCE_FOLDER/WEB-INF $COMPRESSED_FOLDER/
+cp -r $SOURCE_FOLDER/web $COMPRESSED_FOLDER/
+cp -r $SOURCE_FOLDER/pub $COMPRESSED_FOLDER/
+cp $SOURCE_FOLDER/* $COMPRESSED_FOLDER/
 
-java -jar ~/Platypus/bin/Tools.jar -force -minify -app-folder ~/workspace/UiDemo/app/ -processed-file ~/workspace/UiDemo/app/MainView.layout -minified-layouts ~/workspace/uiCompressed/app/MainView.layout
-java -jar ~/Platypus/bin/Tools.jar -force -minify -app-folder ~/workspace/UiDemo/app/ -processed-file ~/workspace/UiDemo/app/MainView.model -minified-models ~/workspace/uiCompressed/app/MainView.model 
-rm -r ~/workspace/uiCompressed/app/Demos.*
-find ~/workspace/uiCompressed/app/StandardWidgets -type f -not -name 'Modules.*' -delete
-find ~/workspace/uiCompressed/app/ModelWidgets -type f -not -name 'Modules.*' -delete
-find ~/workspace/uiCompressed/app/ModelGrid -type f -not -name 'Modules.*' -delete
-find ~/workspace/uiCompressed/app/MenuWidgets -type f -not -name 'Modules.*' -delete
-find ~/workspace/uiCompressed/app/ContainerPanels -type f -not -name 'Modules.*' -delete
-find ~/workspace/uiCompressed/app/Utils -type f -not -name 'Modules.*' -delete
-java -jar ~/Platypus/bin/Tools.jar -index -app-folder ~/workspace/uiCompressed/app/ -processed-folder ~/workspace/uiCompressed/app/ -indexed-modules ~/workspace/uiCompressed/app/Modules.js
+echo "Running require.js optimizer"
+r.js -o build.js
+
+echo "Minify"
+java -jar $TOOLS -minify -app-folder $SOURCE_FOLDER/app/StandardWidgets -processed-folder $SOURCE_FOLDER/app/StandardWidgets -minified-models $COMPRESSED_FOLDER/app/StandardWidgets/Modules.model -minified-layouts $COMPRESSED_FOLDER/app/StandardWidgets/Modules.layout
+java -jar $TOOLS -minify -app-folder $SOURCE_FOLDER/app/ModelWidgets -processed-folder $SOURCE_FOLDER/app/ModelWidgets -minified-models $COMPRESSED_FOLDER/app/ModelWidgets/Modules.model -minified-layouts $COMPRESSED_FOLDER/app/ModelWidgets/Modules.layout
+java -jar $TOOLS -minify -app-folder $SOURCE_FOLDER/app/ModelGrid -processed-folder $SOURCE_FOLDER/app/ModelGrid -minified-models $COMPRESSED_FOLDER/app/ModelGrid/Modules.model -minified-layouts $COMPRESSED_FOLDER/app/ModelGrid/Modules.layout
+java -jar $TOOLS -minify -app-folder $SOURCE_FOLDER/app/MenuWidgets -processed-folder $SOURCE_FOLDER/app/MenuWidgets -minified-models $COMPRESSED_FOLDER/app/MenuWidgets/Modules.model -minified-layouts $COMPRESSED_FOLDER/app/MenuWidgets/Modules.layout
+java -jar $TOOLS -minify -app-folder $SOURCE_FOLDER/app/ContainerPanels -processed-folder $SOURCE_FOLDER/app/ContainerPanels -minified-models $COMPRESSED_FOLDER/app/ContainerPanels/Modules.model -minified-layouts $COMPRESSED_FOLDER/app/ContainerPanels/Modules.layout
+java -jar $TOOLS -minify -app-folder $SOURCE_FOLDER/app/Utils -processed-folder $SOURCE_FOLDER/app/Utils -minified-models $COMPRESSED_FOLDER/app/Utils/Modules.model -minified-layouts $COMPRESSED_FOLDER/app/Utils/Modules.layout
+
+java -jar $TOOLS -force -minify -app-folder $SOURCE_FOLDER/app/ -processed-file $SOURCE_FOLDER/app/MainView.layout -minified-layouts $COMPRESSED_FOLDER/app/MainView.layout
+java -jar $TOOLS -force -minify -app-folder $SOURCE_FOLDER/app/ -processed-file $SOURCE_FOLDER/app/MainView.model -minified-models $COMPRESSED_FOLDER/app/MainView.model 
+
+echo "Remove unnecessary"
+rm -r $COMPRESSED_FOLDER/app/Demos.*
+find $COMPRESSED_FOLDER/app/StandardWidgets -type f -not -name 'Modules.*' -delete
+find $COMPRESSED_FOLDER/app/ModelWidgets -type f -not -name 'Modules.*' -delete
+find $COMPRESSED_FOLDER/app/ModelGrid -type f -not -name 'Modules.*' -delete
+find $COMPRESSED_FOLDER/app/MenuWidgets -type f -not -name 'Modules.*' -delete
+find $COMPRESSED_FOLDER/app/ContainerPanels -type f -not -name 'Modules.*' -delete
+find $COMPRESSED_FOLDER/app/Utils -type f -not -name 'Modules.*' -delete
+echo "Creating index"
+java -jar $TOOLS -index -app-folder $COMPRESSED_FOLDER/app/ -processed-folder $COMPRESSED_FOLDER/app/ -indexed-modules $COMPRESSED_FOLDER/app/Modules.js
+echo 'Now you should add init="Modules.js" nearby  source-path="app" in application-start.html'
 
